21d8b751ab4afc2a029e9d7f4d041f58e8314f4e
sushilbhatta <sushilbhatta58@gmail.com>
2024-05-09 08:51:13 +0545

    feat : added the url search ui

--- app/page.js
+++ app/page.js
@@ -12,7 +12,19 @@ export default function page() {
       >
         <SideBar></SideBar>
         <div className='flex flex-col justify-center items-center gap-3 lg:gap-6'>
-          {/* Component Here */}
+          <div className='bg-gradient-to-r from-[#A4B998] to-[#C6DBBC] rounded-full flex justify-start items-center p-2 pl-4 gap-x-1  lg:p-3 lg:pl-6 lg:gap-x-2  lg:min-w-[268px] '>
+            <span>
+              <IconContext.Provider
+                value={{ size: "1rem", className: "lg:w-[2rem] lg:h-[2rem]" }}
+              >
+                <CiLock />
+              </IconContext.Provider>
+            </span>
+            <p className=' text-base lg:text-2xl align-middle '>
+              https://www.myplant.com//
+            </p>
+          </div>
+          {/* content */}
         </div>
       </main>
     </>
